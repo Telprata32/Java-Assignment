065c2a008472fde0300be3287cbbcef7e19ea4c3
Abdul Rahim <0131352@kdu-online.com>
2021-03-23 22:50:37 +0800

    Minor change to selection of product quantity by user

--- main.java
+++ main.java
@@ -33,15 +33,16 @@ public class main {
 
 		// Prepare needed variables and objects
 		int usChoice, prodQty; // user's selection from the menu
-		double sumPurchase=0; // Total amount of money spent for one transaction
+		double sumPurchase = 0; // Total amount of money spent for one transaction
 		Scanner inScan = new Scanner(System.in); // Scanner for user input
-		
+
 		// Prompt a menu to the user to select a product to purchase
 		System.out.println("Welcome user, please select a product to record it's purchase\n");
 
-		//=========== Begin Main Operation ===================//
+		// =========== Begin Main Operation ===================//
 		// Deploy in a while loop
-		while(true){
+		while (true) {
+
 			System.out.println("1. Toothpaste X\tRM 30.45");
 			System.out.println("2. Toothpaste Y\tRM 26.75");
 			System.out.println("3. Pencil Case\tRM 15.00");
@@ -50,24 +51,29 @@ public class main {
 			System.out.println("\nEnter 0 to settle purchase");
 
 			// Receive user's input
-			System.out.print("Select product: ");
+			System.out.print("\nPunch in product or settle purchase: ");
 			usChoice = inScan.nextInt();
-			inScan.nextLine(); //so that for the next inScan.nextLine, it won't take in an empty line
+			inScan.nextLine(); // so that for the next inScan.nextLine, it won't take in an empty line
 
-			// Prompt user to enter quantity of the product
-			System.out.print("Quantity of product: ");
-			prodQty = inScan.nextInt();
-			inScan.nextLine(); //so that for the next inScan.nextLine, it won't take in an empty line
+			// Prompt user to enter quantity of the product, perform only if the person did not choose to settle the purchase
+			if (usChoice != 0) {
+
+				System.out.print("Quantity of product: ");
+				prodQty = inScan.nextInt();
+				inScan.nextLine(); // so that for the next inScan.nextLine, it won't take in an empty line
+
+			}
 
 			// Calculate the total if the user hasn't chosen to settle purchase
-			if(usChoice == 0){
+			if (usChoice == 0) {
 				break;
-			}	
-			else{
-				sumPurchase += prodQty*returnPrice(usChoice);	
+			} else {
+
+				sumPurchase += prodQty * returnPrice(usChoice);
 			}
+
 		}
-		
+
 		inScan.close(); // close the scanner
 	}
 
