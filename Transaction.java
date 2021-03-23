4fc103c4abc342c2301af5868a8b554f248bd80b
Abdul Rahim <0131352@kdu-online.com>
2021-03-23 21:03:52 +0800

    Added attribute to the Transaction Class

--- Transaction.java
+++ Transaction.java
@@ -6,13 +6,15 @@ public class Transaction {
 	private static int id;
 	private String productName;
 	private int productQty;
+	private double unitPrice;
 	private LocalDate purchaseDate;
 
-	public Transaction(String productName, int productQty) {
+	public Transaction(String productName, int productQty, double unitPrice) {
 
 		// Grab the id from the id from the database and increment it by one
 		// i++; wait for database before uncommenting
 		this.productName = productName;
+		this.unitPrice = unitPrice;
 		this.productQty = productQty;
 		this.purchaseDate = LocalDate.now(); // Retrieve current date and store into the transaction record
 	}
