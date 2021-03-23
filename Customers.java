0891089d3527b1d1a9580c3f75c276fd91b62786
Abdul Rahim <0131352@kdu-online.com>
2021-03-23 13:49:04 +0800

    Initializing files

---
+++ Customers.java
@@ -0,0 +1,9 @@
+package Shopping;
+
+public class Customers {
+	private static int id;
+	private String name;
+	private String address;
+	private String phoneNUm;
+
+}
---
+++ Transaction.java
@@ -0,0 +1,11 @@
+package Shopping;
+
+import java.time.LocalDate;
+
+public class Transaction {
+	private static int id;
+	private String productName;
+	private int productQty;
+	private LocalDate purchaseDate;
+
+}
