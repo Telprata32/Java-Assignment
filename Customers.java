25c989d09feb0ecb557ede5bf94d60432c661d59
Abdul Rahim <0131352@kdu-online.com>
2021-03-23 20:21:42 +0800

    Commits:
    > Initialized Classes
    > Created constructors

--- Customers.java
+++ Customers.java
@@ -4,6 +4,15 @@ public class Customers {
 	private static int id;
 	private String name;
 	private String address;
-	private String phoneNUm;
+	private String phoneNum;
+
+	public Customers(String name, String address, String phoneNum) {
+
+		// Grab the id from the id from the database and increment it by one
+		// i++; wait for database before uncommenting
+		this.name = name;
+		this.phoneNum = phoneNum;
+		this.address = address;
+	}
 
 }
--- Transaction.java
+++ Transaction.java
@@ -8,4 +8,12 @@ public class Transaction {
 	private int productQty;
 	private LocalDate purchaseDate;
 
+	public Transaction(String productName, int productQty) {
+
+		// Grab the id from the id from the database and increment it by one
+		// i++; wait for database before uncommenting
+		this.productName = productName;
+		this.productQty = productQty;
+		this.purchaseDate = LocalDate.now(); // Retrieve current date and store into the transaction record
+	}
 }
