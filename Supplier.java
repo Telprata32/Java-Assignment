e374ac5bb8c1347132585a54f66fe7d4518f4cc0
DeceyvenT <81202384+DeceyvenT@users.noreply.github.com>
2021-03-30 01:36:42 +0800

    Update and rename Customer.java to Supplier.java

---
+++ Supplier.java
@@ -0,0 +1,36 @@
+public class Supplier {
+  private int suppID;
+  private String suppName;
+  private String suppProduct;
+  private int suppQuantity; //thinking
+  private int productID; //extends in transaction class
+    
+  public void setSupplierID (int newSuppID){
+      suppID = newSuppID;
+  }
+    
+  public int getSupplierID (){
+      return suppID;
+  }
+    
+  public void setSupplierName (String newSuppName){
+      suppName = newSuppName;
+  }
+    
+  public String getSupplierName (){
+      return suppName;
+  }
+  
+  public void setproductID (int newProID){
+      productID = newProID;
+  }
+    
+  public int getProductID (){
+      return productID;
+  }
+  
+  public void display(){
+      System.out.println("Supplier ID: " + suppID + "\nSupplyer Name: " + suppName);
+  }
+}
+  
